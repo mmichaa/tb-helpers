2bc40639272f1fb18ab0f650f25ec20b6c382f1f
mmichaa <thexsystem@gmail.com>
2013-04-06 00:10:57 +0200

    many, many fixes; added demo site in dummy-rails-app

--- lib/tb-helpers/base.rb
+++ lib/tb-helpers/base.rb
@@ -9,11 +9,25 @@ module TbHelpers::Base
   BUTTON_SIZES = {:mini => 'btn-mini', :small => 'btn-small', :large => 'btn-large'}
 
   #
+  # essentials
+  #
+
+  def tb_javascript_include_tag(version=nil)
+    version ||= '2.3.1'
+    javascript_include_tag "//netdna.bootstrapcdn.com/twitter-bootstrap/#{version}/js/bootstrap.min.js"
+  end
+
+  def tb_stylesheet_link_tag(version=nil)
+    version ||= '2.3.1'
+    stylesheet_link_tag "//netdna.bootstrapcdn.com/twitter-bootstrap/#{version}/css/bootstrap-combined.min.css"
+  end
+
+  #
   # typography
   #
 
   def tb_lead(content = nil, options = nil, &block)
-    content, options = content_or_options_with_block?(content, options, block)
+    content, options = content_or_options_with_block?(content, options, &block)
     tag = delete_option_if_exists!(options, :tag, :p)
     escape = delete_option_if_exists!(options, :escape, true)
 
@@ -24,7 +38,7 @@ module TbHelpers::Base
   end
 
   def tb_emphasis(style, content = nil, options = nil, &block)
-    content, options = content_or_options_with_block?(content, options, block)
+    content, options = content_or_options_with_block?(content, options, &block)
     tag = case style
       when :small then :small
       when :bold then :strong
@@ -34,6 +48,7 @@ module TbHelpers::Base
     end
     escape = delete_option_if_exists!(options, :escape, true)
 
+    classes = []
     EMPHASIS_CLASSES.each do |class_sym, class_name|
       class_val = delete_option_if_exists!(options, class_sym, false)
       classes << class_name if class_val
@@ -44,20 +59,20 @@ module TbHelpers::Base
   end
 
   def tb_small(content = nil, options = nil, &block)
-    emphasis(:small, content, options, &block)
+    tb_emphasis(:small, content, options, &block)
   end
 
   def tb_bold(content = nil, options = nil, &block)
-    emphasis(:bold, content, options, &block)
+    tb_emphasis(:bold, content, options, &block)
   end
 
   def tb_italics(content = nil, options = nil, &block)
-    emphasis(:italics, content, options, &block)
+    tb_emphasis(:italics, content, options, &block)
   end
   alias :tb_italic :tb_italics
 
   def tb_abbreviation(content = nil, options = nil, &block)
-    content, options = content_or_options_with_block?(content, options, block)
+    content, options = content_or_options_with_block?(content, options, &block)
     tag = delete_option_if_exists!(options, :tag, :abbr)
     escape = delete_option_if_exists!(options, :escape, true)
 
@@ -66,7 +81,7 @@ module TbHelpers::Base
   alias :tb_abbr :tb_abbreviation
 
   def tb_address(content = nil, options = nil, &block)
-    content, options = content_or_options_with_block?(content, options, block)
+    content, options = content_or_options_with_block?(content, options, &block)
     tag = delete_option_if_exists!(options, :tag, :address)
     escape = delete_option_if_exists!(options, :escape, true)
 
@@ -74,7 +89,7 @@ module TbHelpers::Base
   end
 
   def tb_blockquote(content = nil, options = nil, &block)
-    content, options = content_or_options_with_block?(content, options, block)
+    content, options = content_or_options_with_block?(content, options, &block)
     tag = delete_option_if_exists!(options, :tag, :blockquote)
     escape = delete_option_if_exists!(options, :escape, true)
 
@@ -86,7 +101,7 @@ module TbHelpers::Base
   #
 
   def tb_code_inline(content = nil, options = nil, &block)
-    content, options = content_or_options_with_block?(content, options, block)
+    content, options = content_or_options_with_block?(content, options, &block)
     tag = delete_option_if_exists!(options, :tag, :code)
     escape = delete_option_if_exists!(options, :escape, true)
 
@@ -94,7 +109,7 @@ module TbHelpers::Base
   end
 
   def tb_code_block(content = nil, options = nil, &block)
-    content, options = content_or_options_with_block?(content, options, block)
+    content, options = content_or_options_with_block?(content, options, &block)
     tag = delete_option_if_exists!(options, :tag, :pre)
     escape = delete_option_if_exists!(options, :escape, true)
 
@@ -110,7 +125,7 @@ module TbHelpers::Base
   # tables
   #
 
-  def tb_table(options, &block)
+  def tb_table(options = nil, &block)
     options ||= {}
     tag = delete_option_if_exists!(options, :tag, :table)
     escape = delete_option_if_exists!(options, :escape, true)
@@ -129,11 +144,12 @@ module TbHelpers::Base
     end
   end
 
-  def tb_table_row(options, &block)
+  def tb_table_row(options = nil, &block)
     options ||= {}
     tag = delete_option_if_exists!(options, :tag, :tr)
     escape = delete_option_if_exists!(options, :escape, true)
 
+    # FIXME this should be done with context option
     classes = []
     TABLE_ROW_CLASSES.each do |class_sym, class_name|
       class_val = delete_option_if_exists!(options, class_sym, false)
@@ -147,6 +163,10 @@ module TbHelpers::Base
   #
   # forms
   #
+
+  def tb_form_for(*args)
+    raise NotImplementedError.new
+  end
 
   def tb_fieldset(options = nil, &block)
     options ||= {}
@@ -187,7 +207,7 @@ module TbHelpers::Base
   end
 
   def tb_control_label(content = nil, options = nil, &block)
-    content, options = content_or_options_with_block?(content, options, block)
+    content, options = content_or_options_with_block?(content, options, &block)
     options = set_or_prepend_option!(options, :class, 'control-label')
     tag = delete_option_if_exists!(options, :tag, :label)
     escape = delete_option_if_exists!(options, :escape, true)
@@ -207,6 +227,7 @@ module TbHelpers::Base
     set_or_prepend_option!(options, :class, classes.join(' '))
     content_tag(tag, options, nil, escape, &block)
   end
+  alias :tb_check_box_label :tb_checkbox_label
 
   def tb_radio_label(options = nil, &block)
     options ||= {} # set_or_prepend_option!(options, :class, 'radio')
@@ -246,20 +267,20 @@ module TbHelpers::Base
   end
 
   def tb_input_addon_prepend(options = nil, &block)
-    input_addon(:prepend, options, &block)
+    tb_input_addon(:prepend, options, &block)
   end
 
   def tb_input_addon_append(options = nil, &block)
-    input_addon(:append, options, &block)
+    tb_input_addon(:append, options, &block)
   end
 
   def tb_input_addon_pre_and_append(options = nil, &block)
-    input_addon(:combined, options, &block)
+    tb_input_addon(:combined, options, &block)
   end
   alias :tb_input_addon_combined :tb_input_addon_pre_and_append
 
   def tb_input_uneditable(content = nil, options = nil, &block)
-    content, options = content_or_options_with_block?(content, options, block)
+    content, options = content_or_options_with_block?(content, options, &block)
     tag = delete_option_if_exists!(options, :tag, :span)
     escape = delete_option_if_exists!(options, :escape, true)
 
@@ -281,7 +302,7 @@ module TbHelpers::Base
   end
 
   def tb_input_help(style, content = nil, options = nil, &block)
-    content, options = content_or_options_with_block?(content, options, block)
+    content, options = content_or_options_with_block?(content, options, &block)
     tag = delete_option_if_exists!(options, :tag, :span)
     escape = delete_option_if_exists!(options, :escape, true)
 
@@ -297,11 +318,11 @@ module TbHelpers::Base
   end
 
   def tb_input_help_inline(content = nil, options = nil, &block)
-    input_help(:inline, content = nil, options = nil, &block)
+    tb_input_help(:inline, content, options, &block)
   end
 
   def tb_input_help_block(content = nil, options = nil, &block)
-    input_help(:block, content = nil, options = nil, &block)
+    tb_input_help(:block, content, options, &block)
   end
 
   #
@@ -317,7 +338,7 @@ module TbHelpers::Base
   end
 
   def tb_button(content = nil, options = nil, &block)
-    content, options = content_or_options_with_block?(content, options, block)
+    content, options = content_or_options_with_block?(content, options, &block)
     tag = delete_option_if_exists!(options, :tag, :div)
     escape = delete_option_if_exists!(options, :escape, true)
 
--- test/dummy/app/helpers/application_helper.rb
+++ test/dummy/app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def render_tb_helper_methode(simple_methode, simple_args)
+    simple_args ||= ["Using #{simple_methode.to_s}() here"]
+    simple_block = nil
+
+    if simple_args.last.is_a?(Proc)
+      simple_block = simple_args.pop
+    elsif simple_args.last == :partial
+      simple_args.pop
+      simple_block = Proc.new { render(partial: simple_methode.to_s) }
+    end
+
+    if simple_args.count > 0
+      send(simple_methode, *simple_args, &simple_block)
+    else
+      capture(&simple_block)
+    end
+  end
 end
